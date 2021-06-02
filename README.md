32b50e6c5aae6bcee3d5a3d267197788f5a06e80
Jack Dougherty <jack.dougherty@trincoll.edu>
2021-06-02 10:44:34 -0400

    new published Google sheet

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ## Live links (replace with your own)
 - Leaflet Map https://jackdougherty.github.io/coa-grants-map/
-- Google Sheets template https://docs.google.com/spreadsheets/d/1pAu2oNz6-apYUCEPNc8dC9OwGMTVwMOQVws4LlSWz10/edit#gid=842189854
+- Google Sheets template https://docs.google.com/spreadsheets/d/1Knh8x9U3COUSrvsZpTxdePIUDlQmgvl5jDLdTioS5HA/edit#gid=0
 
 ## Build your own
 https://handsondataviz.org
--- google-doc-url.js
+++ google-doc-url.js
@@ -1,5 +1,5 @@
 // paste in your published Google Sheets URL from the browser address bar
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1pAu2oNz6-apYUCEPNc8dC9OwGMTVwMOQVws4LlSWz10/edit#gid=842189854';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1Knh8x9U3COUSrvsZpTxdePIUDlQmgvl5jDLdTioS5HA/edit#gid=0';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
 var googleApiKey = 'AIzaSyBh9nKnVZm2RPeZa0ywCOxPAgJJfK87WhY';
